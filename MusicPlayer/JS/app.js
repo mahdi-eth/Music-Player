20ce234404c0385e747f762b3f21deb53b74a017
Mahdi EttehadNejad <99660553+mahdi-Eth@users.noreply.github.com>
2022-08-23 15:46:48 +0430

    Add files via upload

--- MusicPlayer/JS/app.js
+++ MusicPlayer/JS/app.js
@@ -4,6 +4,7 @@ import { playPauser }  from "./jsModule/currentStationClick.js";
 import {timer} from "./jsModule/handleTiming.js";
 import progressBarRanger from "./jsModule/handleProgressBar.js";
 import { progressBarWidth } from "./jsModule/handleProgressBar.js";
+import { nextBtn } from "./jsModule/nextPrevBtns.js";
 
 timer();
 setInterval( () => {
--- MusicPlayer/JS/jsModule/handleCurrentStation.js
+++ MusicPlayer/JS/jsModule/handleCurrentStation.js
@@ -4,9 +4,9 @@ import { musicStationClicker } from "./currentStationClick.js";
 import { playPauser } from "./currentStationClick.js";
 import { playPauserIfElse } from "./currentStationClick.js";
 import { pauseEl } from "./currentStationClick.js";
-import {timer} from "./handleTiming.js";
-// import progressBarRanger from "./handleProgressBar.js";
-
+import { timer } from "./handleTiming.js";
+import { nextBtn } from "./nextPrevBtns.js";
+import { prevBtn } from "./nextPrevBtns.js";
 
 const musicStationCover = document.querySelector(".current-music-cover");
 const musicStationSinger = document.querySelector(".current-music-singer");
@@ -15,18 +15,60 @@ export const audio = document.querySelector("#audio");
 
 export default [...containMusicCards.children].forEach((card) => {
   card.addEventListener("click", () => {
-    const filtredStation = musics().filter(
+    let filtredStation = musics().filter(
       (item) => item.id === Number(card.dataset.id)
     )[0];
-    musicStationCover.style.background = `url(${filtredStation.cover})`;
-    musicStationSinger.innerHTML = filtredStation.artist;
-    musicStationTitle.innerHTML = filtredStation.name;
-    audio.src = filtredStation.audio;
-    audio.play();
-    timer()
-    musicStationClicker.innerHTML = pauseEl;
-    musicStationClicker.style.display = "inline";
-    playPauser(playPauserIfElse(playPauser()));
+
+    let indexOfCurrentMusic = musics().findIndex(
+      (item) => item.id === Number(card.dataset.id)
+    );
+
+    function rendering1(e) {
+      musicStationCover.style.background = `url(${filtredStation.cover})`;
+      musicStationSinger.innerHTML = filtredStation.artist;
+      musicStationTitle.innerHTML = filtredStation.name;
+      audio.src = filtredStation.audio;
+      audio.play();
+      timer();
+      musicStationClicker.innerHTML = pauseEl;
+      musicStationClicker.style.display = "inline";
+      playPauser(playPauserIfElse(playPauser()));
+    }
+
+    rendering1();
+
+    function rendering2(e) {
+      filtredStation = musics()[indexOfCurrentMusic];
+      musicStationCover.style.background = `url(${filtredStation.cover})`;
+      musicStationSinger.innerHTML = filtredStation.artist;
+      musicStationTitle.innerHTML = filtredStation.name;
+      audio.src = filtredStation.audio;
+      audio.play();
+      timer();
+      musicStationClicker.innerHTML = pauseEl;
+      musicStationClicker.style.display = "inline";
+      playPauser(playPauserIfElse(playPauser()));
+      // rendering1 = rendering2;
+    }
+
+    nextBtn.addEventListener("click", (event) => {
+      if (indexOfCurrentMusic !== 6) {
+        indexOfCurrentMusic++;
+        rendering2(musics()[indexOfCurrentMusic]);
+      } else if (indexOfCurrentMusic == 6) {
+        indexOfCurrentMusic = 0;
+        rendering2(musics()[indexOfCurrentMusic]);
+      }
+    });
+
+    prevBtn.addEventListener("click", (event) => {
+      if (indexOfCurrentMusic !== 0) {
+        indexOfCurrentMusic--;
+        rendering2(musics()[indexOfCurrentMusic]);
+      } else if (indexOfCurrentMusic == 0) {
+        indexOfCurrentMusic = 6;
+        rendering2(musics()[indexOfCurrentMusic]);
+      }
+    });
   });
 });
-
